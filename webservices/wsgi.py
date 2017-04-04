502805d421e4d2ba62e05ecc5c7fd1dd970331a3
Dmytryi <dmytryi.striletskyi@gmail.com>
2017-04-05 02:21:12 +0300

    Remove unnecessary code

--- webservices/wsgi.py
+++ webservices/wsgi.py
@@ -6,9 +6,6 @@ It exposes the WSGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
-# import os
-#
-# os.environ['DJANGO_SETTINGS_MODULE'] = 'webservices.settings'
 
 import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "webservices.settings")
