d7010b2d6c248cb251e033790070039e75fb05be
Dmytryi <dmytryi.striletskyi@gmail.com>
2017-04-09 22:54:21 +0300

    Add google-oauth2 core code

--- ec2/urls.py
+++ ec2/urls.py
@@ -1,9 +1,14 @@
+from django.contrib.auth.decorators import login_required
 from django.conf.urls import url
+
 from . import views
+
 
 app_name = 'ec2'
 
 urlpatterns = [
     url(r'^$', views.Homepage.as_view(), name='index'),
-    url(r'^(?P<user>\w+)/(?P<instance>[-\w\+%_&]+)/$', views.Instance.as_view(), name='instance'),
+    url(r'^login_error/$', views.LoginError.as_view(), name='login_error'),
+    url(r'^after_login_redirect/$', login_required(views.AfterLoginRedirect.as_view()), name='after_login_redirect'),
+    url(r'^(?P<instance>[-\w\+%_&]+)/$', login_required(views.Instance.as_view()), name='instance')
 ]
